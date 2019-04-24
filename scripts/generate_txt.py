6d9c157dd2c8e3ae5c0ebfeb724609f84842f44b
EC2 Default User <ec2-user@ip-172-31-48-112.ec2.internal>
2019-04-24 08:42:52 +0000

    Script to generate txt from images

--- scripts/generate_txt.py
+++ scripts/generate_txt.py
@@ -5,7 +5,9 @@ import sys
 images  = os.listdir(sys.argv[1])
 all_image_names = []
 for image in images:
-	all_image_names.append(os.path.join(sys.argv[1], image))
+	if image.split('.')[1] == "jpg":
+		all_image_names.append(os.path.join(sys.argv[1], image))
 
+print(len(all_image_names))
 numpy.savetxt(os.path.join(sys.argv[1], "list.txt"), all_image_names, fmt='%s')
 
